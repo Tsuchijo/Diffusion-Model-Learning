86f3369fac6f3ee92b4291e5bc220ae189b1308e
Tsuchijo <TsuchitoriJoel@gmail.com>
2023-10-09 17:47:06 -0700

    hacked together working model, commented out wandb logging for now

--- diffusion_models/data.py
+++ diffusion_models/data.py
@@ -1,15 +1,41 @@
 import numpy as np
 import torch
-from synthetic_datasets import toy_2d
+#from synthetic_datasets import toy_2d
 from torch.utils.data import Dataset
+from common_utils.random import RNG
+
+
+## Manually add dataset to get over arg error
+class ToyDatasetBase(Dataset):
+    def __init__(self, *args, **kwargs):
+        self.setup_data(*args, **kwargs)
+
+    def setup_data(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        return self.data[index]
+
+class Checkerboard(ToyDatasetBase):
+    def setup_data(self, n_samples):
+        x1 = np.random.rand(n_samples) * 4 - 2
+        x2_ = np.random.rand(n_samples) - np.random.randint(0, 2, n_samples) * 2
+        x2 = x2_ + (np.floor(x1) % 2)
+        data = np.concatenate([x1[:, None], x2[:, None]], 1) / 2
+        self.data = data
 
 
 def get_train_set(dataset_name, n_samples=100000, slack=None):
-    return toy_2d.get_dataset(dataset_name, n_samples=n_samples, seed=123)
+    with RNG(456):
+        return Checkerboard(n_samples=n_samples)
 
 
 def get_test_set(dataset_name, n_samples=1000, slack=None):
-    return toy_2d.get_dataset(dataset_name, n_samples=n_samples, seed=456)
+    with RNG(123):
+        return Checkerboard(n_samples=n_samples)
 
 
 def get_datasets(data_config):
--- scripts/train.py
+++ scripts/train.py
@@ -8,6 +8,7 @@ from common_utils.random import RNG, set_random_seed
 from ml_collections.config_flags import config_flags
 from tqdm.auto import tqdm
 import itertools
+import numpy as np
 
 import diffusion_models
 from diffusion_models import data
@@ -22,7 +23,27 @@ flags.DEFINE_list("tags", [], "Tags to add to the run.")
 flags.DEFINE_string("wandb_name", None, "wandb name.")
 flags.mark_flags_as_required(["config"])
 
+## Define the model we are training ##
+#  2D checkerboard dataset 3d -> 2d (x,y, t) -> (x,y)
+#  fully connected network MLP in between
 
+class MLP_diffusion(torch.nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.net = torch.nn.Sequential(
+            torch.nn.Linear(3, 256),
+            torch.nn.ReLU(),
+            torch.nn.Linear(256, 256),
+            torch.nn.ReLU(),
+            torch.nn.Linear(256, 2),
+        )
+        self.double()
+    
+    def forward(self, x):
+        return self.net(x)
+
+
+## Training loop ##
 def train(config):
     if config.seed is not None:
         set_random_seed(config.seed)
@@ -37,17 +58,60 @@ def train(config):
     )
     inf_train_loader = itertools.cycle(train_loader)
 
+    ## define beta and alpha ##
+    # beta is a linear function of the timestep 0 < beta_0 < beta_T < 1
+    # is cumprod of 1-beta from 0 to T
+    # T is total number of timesteps
+    t_total = 1000
+    betas = np.linspace(1e-4, 1.0, t_total)
+    alphas = torch.cumprod(1 - torch.tensor(betas), 0)
+
     ## Load the model ##
+    model = MLP_diffusion()
+    # randomize weights
+    for p in model.parameters():
+        torch.nn.init.normal_(p, 0, 0.05)
+
+    ## Load the optimizer ##
+    optimizer = torch.optim.Adam(model.parameters(), lr=config.optim.lr)
+
     ## Training loop ##
     for iteration in range(config.training.n_iters):
         x0 = next(inf_train_loader)
+        # forward pass
+        # define timestep by random sampling from 1 - t_total
+        timestep = torch.randint(1, t_total, (x0.shape[0], 1))
+        x_t, epsilon = forward_pass(x0, timestep, alphas)
+        # backward pass
+        # append timestep to x_t
+        x_t = torch.cat([x_t, timestep], axis=1)
+        eps_pred = model(x_t)
+        loss = torch.nn.functional.mse_loss(eps_pred, epsilon)
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+        print(loss.item())
+        #wandb.log({"loss": loss.item()})
+        
+
+## Forward pass of the diffusion model using normal distribution of noise
+# takes an input image then applies forward diffusion process to it
+def forward_pass( x0, timesteps, alpha):
+    # x0 is the input image, uses precalculated alpha_t from timeskipping
+    # alpha_bar is the cumulative product of beta_t - 1 from 0 to t where t is the current timestep
+    # calculate noise from normal distribution (epsilon)
+    epsilon = torch.randn_like(x0)
+    a_t = alpha[timesteps]
+    x_t = torch.sqrt(1 - a_t) * x0 + torch.sqrt(a_t) * epsilon
+    return x_t, epsilon
 
 
 
 def main(argv):
-    logging.init(config=FLAGS.config.to_dict(), tags=FLAGS.tags, name=FLAGS.wandb_name)
+    #logging.init(config=FLAGS.config.to_dict(), tags=FLAGS.tags, name=FLAGS.wandb_name)
     train(FLAGS.config)
-    wandb.log({})
+    #wandb.log({})
+    
 
 
 if __name__ == "__main__":
